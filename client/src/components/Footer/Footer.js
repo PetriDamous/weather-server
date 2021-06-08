88de57853316f31ab4cb9290de748d895b8c7148
dwilliams64 <dwilliams_64@yahoo.com>
2021-06-08 14:11:42 -0500

    Added api logos to footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,8 +5,24 @@ import "./Footer.scss";
 export default function Footer() {
   return (
     <div className="md-space-top">
-      <footer className="footer">
+      <footer className="footer flow-content">
         <p>Powred by the PetriD stack</p>
+        <div className="split">
+          <a
+            href="https://openweathermap.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="img/OpenWeather-Logo.jpg" />
+          </a>
+          <a
+            href="https://www.mapbox.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="img/Mapbox_logo_2019.svg" />
+          </a>
+        </div>
       </footer>
     </div>
   );
---
+++ data.md
@@ -0,0 +1,17 @@
+## Old data
+
+weather_descriptions: [ 'Partly cloudy' ],
+wind_speed: 16,
+wind_degree: 160,
+wind_dir: 'SSE',
+pressure: 1014,
+precip: 0,
+humidity: 73,
+cloudcover: 75,
+feelslike: 99,
+uv_index: 6,
+visibility: 10,
+is_day: 'yes'
+}
+
+## New Data
